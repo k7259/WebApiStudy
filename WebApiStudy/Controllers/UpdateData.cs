07a12a9e00ade9a28475523a5e9d042ef9d5e6b1
k7259 <sanjaykumar7259@gmail.com>
2023-04-08 11:45:31 +0530

    newupdate

---
+++ WebApiStudy/Controllers/UpdateData.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiStudy.ModelFolder;
+
+namespace WebApiStudy.Controllers
+{
+
+	//public class UpdateData : ControllerBase
+
+	//{
+	//	ApplicationDB dB;
+	//	public UpdateData(ApplicationDB Update)
+	//	{
+	//		this.dB = Update;
+	//	}
+	//	[HttpPut]
+	//	[Route(template: "UpdatePlayer")]
+	//	public IActionResult UpdatePlayer (IplPlayer player)
+	//	{ 
+
+	//	}
+
+
+	//}
+}
+
+		
